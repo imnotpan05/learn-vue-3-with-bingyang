08d70c5f53f63cbf68a660ac44eff7d665a733b2
An Cao <caophuongan@gmail.com>
2025-03-09 18:12:47 -0500

    Vue Props is here!

---
+++ src/BlogPost.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="blog-post">
+      <p>
+        {{ message }}
+      </p>
+      <h2>{{ id }} - {{ blogPostTitle }}</h2>
+      <h4>{{ blogPostContent }}</h4>
+        <button @click="changeBlogPostTitle">Change blog post title</button>
+
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  let message = ref('This is the BlogPost component.')
+  //define props here:
+  let props = defineProps(['id', 'blogPostTitle', 'blogPostContent'])
+ function changeBlogPostTitle(){
+    props.blogPostTitle = 'A different title'
+ }
+
+  </script>
+  
+  <style scoped>
+  .blog-post {
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+  </style>
